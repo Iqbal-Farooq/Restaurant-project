e6fedce2ff3066ff9875b224e181b3bca45c842b
iqbal-Farooq <akashfarooq222@gmail.com>
2023-01-02 14:46:27 +0530

    FIXED THE BUG IN THE CART

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,12 +4,17 @@ import CartContext from '../../Store/Cart-context';
 import { useContext } from 'react';
 import CartItem from './CartItem';
 
+import { useReducer } from "react";
+
+
+
+
 
 const Cart=props=>{
     const cartCntx=useContext(CartContext)
     
     const CartItemRemovehandler=id=>{cartCntx.removeItem(id)};
-    const CartItemAddHandler=item=>{cartCntx.addItem(item)};
+    const CartItemAddHandler=item=>{cartCntx.addItem({...item,quantity:1})};
 
     const cartitems=(<ul className={classes['cart-items']}>
     {cartCntx.items.map((item)=><CartItem key={item.id} id={item.id} name={item.name} quantity={Number(item.quantity)} price={item.price} 
